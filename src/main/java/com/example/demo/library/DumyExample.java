b297869ea0f95e6ed5943b84951df30ad632b94a
Jhonconnors <anonimat.jk@gmail.com>
2024-03-06 01:30:33 -0300

    add parameters publish

---
+++ src/main/java/com/example/demo/library/DumyExample.java
@@ -0,0 +1,8 @@
+package com.example.demo.library;
+
+public class DumyExample {
+
+    public String getLibraryText(String text){
+        return "this is the library text with your text :"+text;
+    }
+}
